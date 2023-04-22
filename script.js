b32ad772195b4f60b24d424081a021a1902f30b3
Kaysan Mohamed Rifath <127319321+KaysanMR@users.noreply.github.com>
2023-04-22 12:21:21 +0500

    Add replay interface

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const CHOICES = ['rock', 'paper', 'scissors']
 let playerScore = 0;
 let computerScore = 0;
+const maxScore = 3;
 
 const delay = ms => new Promise(res => setTimeout(res, ms)); //util for async delay
 
@@ -10,14 +11,18 @@ const initialize = () => {
     playerScore = 0;
     computerScore = 0;
 
+    endScore.textContent = '';
     message.textContent = 'Welcome to Rock Paper Scissors!';
     log.textContent = 'choose an option to begin :)'
     scoreboard.innerHTML = (`you: ${playerScore} computer: ${computerScore}`);
-    hide('.start');
+    hide('.menu');
     show('.game');
 };
 
 const message = document.querySelector('.message');
+
+const replayMessage = document.querySelector('.replayMessage');
+replayMessage.textContent = '';
 
 const log = document.querySelector('.log');
 
@@ -31,21 +36,27 @@ function getComputerChoice() {
 const start = document.querySelector('.start');
 start.addEventListener('click',() => initialize());
 
-//game function
-const button = document.querySelectorAll('#btn');
-button.forEach(button => {
-    button.addEventListener('click',(e) => {
-        let playerChoice = (e.target.innerHTML.toLowerCase());
-        let computerChoice = getComputerChoice();
-        
-        round(playerChoice,computerChoice);
 
-        message.textContent = result;
-        log.innerHTML = (`You chose ${playerChoice}, <n> The computer chose ${computerChoice}.`);
-        scoreboard.innerHTML = (`you: ${playerScore} computer: ${computerScore}`);
-        end();
+
+game();
+
+//game function
+function game() {
+    const button = document.querySelectorAll('#btn');
+    button.forEach(button => {
+        button.addEventListener('click',(e) => {
+            let playerChoice = (e.target.innerHTML.toLowerCase());
+            let computerChoice = getComputerChoice();
+            
+            round(playerChoice,computerChoice);
+    
+            message.textContent = result;
+            log.innerHTML = (`You chose ${playerChoice}, <n> The computer chose ${computerChoice}.`);
+            scoreboard.innerHTML = (`you: ${playerScore} computer: ${computerScore}`);
+            end();
+        });
     });
-});
+};
 
 
 function hide(element) {
@@ -89,19 +100,25 @@ function round(playerChoice, computerChoice) {
     return result;
 };
 
+const endScore = document.querySelector('.endScore');
+
+const score = () => {
+    const score = document.createTextNode(`you: ${playerScore} computer: ${computerScore}`);
+    endScore.appendChild(score);
+};
+
 const end = async () => {
     const reset = () => {
         hide('.game');
-        show('.start');
-
-    };
-    if (playerScore >= 5) {
-        await delay(300);
-        alert('You Win!');
+        show('.menu');
+        score();
+        start.textContent = 'Play Again?'
+    }
+    if (playerScore >= maxScore) {
+        replayMessage.textContent = 'You won!';
         reset();
-    } else if (computerScore >= 5) {
-        await delay(300);
-        alert ('You Lose!');
+    } else if (computerScore >= maxScore) {
+        replayMessage.textContent = 'You lost!';
         reset();
     };
 };
